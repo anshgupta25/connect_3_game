8314a2d207a1130c07fd175943e2eefc22163693
hiten123-ui <vidhani.hiten2001@gmail.com>
2021-03-22 01:15:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/connect3/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.connect3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    int activePlayer = 0;
+    // 0 yellow, 1 red
+    public void dropin(View view)
+    {
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+        Log.i("Tag", counter.getTag().toString());
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+
+            counter.animate().rotationBy(3600).translationYBy(1500);
+
+            activePlayer = 1;
+        }
+        else{
+            counter.setImageResource(R.drawable.red);
+
+            counter.animate().rotationBy(3600).translationYBy(1500);
+
+            activePlayer = 0;
+
+        }
+        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
